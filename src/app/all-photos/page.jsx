d03b150fc9890784eede51b07d7464065fcb05d8
AtikHasanSarker <atiksagor24@gmail.com>
2026-05-02 02:14:42 +0600

    Added Category filtering

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,22 +1,31 @@
+import Category from "@/components/Category";
+import PhotoCard from "@/components/PhotoCard";
 
-import PhotoCard from '@/components/ui/PhotoCard';
-import React from 'react';
+const AllPhotosPage = async ({ searchParams }) => {
+  const { category } = await searchParams;
+  console.log(category);
+  const res = await fetch("https://pixgen-eta.vercel.app/data.json");
+  const photos = await res.json();
 
-const AllPhotos = async () => {
-    const generations = await fetch(
-      "https://pixgen-lake.vercel.app/data.json"
-    )
-    const photos = await generations.json();
-    console.log("from all photos", photos);
-    return (
-        <div className="my-10">
-            <h1 className="text-2xl font-bold my-5">All Photos</h1>
+  const filteredPhotos = category
+    ? photos.filter(
+        (photo) => photo.category.toLowerCase() == category.toLowerCase(),
+      )
+    : photos;
 
-            <div className="grid grid-cols-4 gap-5">
-                {photos.map(photo => <PhotoCard key={photo.id} photo={photo} />)}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-2xl font-bold m-4">All Photos</h1>
+
+      <Category />
+
+      <div className="grid grid-cols-4 gap-5">
+        {filteredPhotos.map((photo) => (
+          <PhotoCard key={photo.id} photo={photo} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default AllPhotos;
+export default AllPhotosPage;
